c3c254853d392bfcb0d453e13f42943fa1f77402
fsck <fsck66@users.noreply.github.com>
2015-05-07 00:46:01 -0400

    ...

---
+++ iOS_HomeOwner_App_numberpad_keyboardbehaviour_pushingMoreControllers_Challenge_CHAP10/ZGCDateCreatedViewController.h
@@ -0,0 +1,16 @@
+//
+//  ZGCDateCreatedViewController.h
+//  iOS_HomeOwner_App_numberpad_keyboardbehaviour_pushingMoreControllers_Challenge_CHAP10
+//
+//  Created by EvilKernel on 4/28/15.
+//  Copyright (c) 2015 Zerogravity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class ZGCItem;
+
+@interface ZGCDateCreatedViewController : UIViewController
+
+@property (nonatomic, strong) ZGCItem *item;
+
+@end
